4ddd1fbd06d749a67da6bd9075d7b1ec107609c1
mdingemanse <mdingemanse@users.noreply.github.com>
2023-06-15 22:30:38 +0200

    reapply my customizations

--- lib/shortcode/shortcode.request.php
+++ lib/shortcode/shortcode.request.php
@@ -627,8 +627,8 @@ function Zotpress_shortcode_request( $checkcache = false )
 					{
          				$item->bib = str_ireplace(
    								"doi:" . $item->data->DOI,
-   								"<a ".$zp_target_output."href='http://doi.org/".$item->data->DOI."'>http://doi.org/".$item->data->DOI."</a>",
-   								$item->bib
+						"<a class='zp-doi-link' ".$zp_target_output."href='https://doi.org/".$item->data->DOI."'>".$item->data->DOI."</a>",
+						$item->bib
    							);
      				}
 					// Styles with http
@@ -637,8 +637,8 @@ function Zotpress_shortcode_request( $checkcache = false )
 					{
          				$item->bib = str_ireplace(
    								"http://doi.org/" . $item->data->DOI,
-   								"<a ".$zp_target_output."href='http://doi.org/".$item->data->DOI."'>http://doi.org/".$item->data->DOI."</a>",
-   								$item->bib
+						"doi: <a class='zp-doi-link' ".$zp_target_output."href='https://doi.org/".$item->data->DOI."'>".$item->data->DOI."</a>",
+						$item->bib
    							);
 					}
 					// HTTPS format
@@ -647,7 +647,7 @@ function Zotpress_shortcode_request( $checkcache = false )
 					{
          				$item->bib = str_ireplace(
    								"https://doi.org/" . $item->data->DOI,
-   								"<a ".$zp_target_output."href='https://doi.org/".$item->data->DOI."'>https://doi.org/".$item->data->DOI."</a>",
+						"doi: <a class='zp-doi-link' ".$zp_target_output."href='https://doi.org/".$item->data->DOI."'>".$item->data->DOI."</a>",
    								$item->bib
    							);
  					}
